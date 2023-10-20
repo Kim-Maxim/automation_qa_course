2d6f38bebc463e5e3d9e8925d635d7742273a16f
Maxim Kim <kimmaxim69@gmail.com>
2023-10-20 10:59:19 +0300

    add_generator_data

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -2,20 +2,27 @@ import time
 
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
+from generator.generator import generated_person
 
 class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys("Ivan Petrov")
-        self.element_is_visible(self.locators.EMAIL).send_keys("ivanpetrov@gmail.com")
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys("Russia")
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys("Moscow")
+        person_info = next(generated_person())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address, permanent_address
 
     def check_filled_form(self):
-        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text
-        email = self.element_is_present(self.locators.CREATED_EMAIL).text
-        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text
-        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
         return full_name, email, current_address, permanent_address
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -7,10 +7,10 @@ class TestElemets:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
             text_box_page.open()
-            text_box_page.fill_all_fields()
-            output_name, output_email, output_current_adress, output_permanent_adress = text_box_page.check_filled_form()
-            print(output_name)
-            print(output_email)
-            print(output_current_adress)
-            print(output_permanent_adress)
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
+            assert full_name == output_name, "the full_name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_current_address, "the current_address does not match"
+            assert permanent_address == output_permanent_address, "the permanent_address does not match" 
 
