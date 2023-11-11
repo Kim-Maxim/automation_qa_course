197a465b28f8575cdf7a0fd714252504ac9bdc2b
Maxim Kim <kimmaxim69@gmail.com>
2023-11-11 12:53:12 +0300

    Добавил класс и тесты для Frames

--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -1,7 +1,7 @@
 import random
 import time
 from pages.base_page import BasePage
-from locators.alerts_frame_windows_locators import AlertsPageLocators, BrowserWindowsPageLocators
+from locators.alerts_frame_windows_locators import AlertsPageLocators, BrowserWindowsPageLocators, FramesPageLocators
 
 
 class BrowserWindowsPage(BasePage):
@@ -50,6 +50,30 @@ class AlertsPage(BasePage):
         alert_window.accept()
         text_result = self.element_is_present(self.locators.PROMPT_RESULT).text
         return text, text_result
+    
+class FramesPage(BasePage):
+
+    locators = FramesPageLocators()
+
+    def check_frame(self, frame_num):
+        if frame_num == 'frame1':
+            frame = self.element_is_present(self.locators.FIRST_FRAME)
+            width = frame.get_attribute('width')
+            height = frame.get_attribute('height')
+            self.driver.switch_to.frame(frame)
+            text = self.element_is_present(self.locators.TITLE_FRAME).text
+            self.driver.switch_to.default_content()
+            return [text, width, height]
+        
+        if frame_num == 'frame2':
+            frame = self.element_is_present(self.locators.SECOND_FRAME)
+            width = frame.get_attribute('width')
+            height = frame.get_attribute('height')
+            self.driver.switch_to.frame(frame)
+            text = self.element_is_present(self.locators.TITLE_FRAME).text
+            self.driver.switch_to.default_content()
+            return [text, width, height]   
+
 
     
 
--- tests/alerts_frame_windows_tests.py
+++ tests/alerts_frame_windows_tests.py
@@ -1,5 +1,5 @@
 import pytest
-from pages.alerts_frame_windows_page import AlertsPage, BrowserWindowsPage
+from pages.alerts_frame_windows_page import AlertsPage, BrowserWindowsPage, FramesPage
 
 
 class TestAlertsFrameWindow:
@@ -9,13 +9,13 @@ class TestAlertsFrameWindow:
             new_tab_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
             new_tab_page.open()
             text_result = new_tab_page.check_opened_new_tab()
-            assert text_result == "This is a sample page", 'text_result is not equal text_title'
+            assert text_result == "This is a sample page", 'The new window has not opened or an incorrect window'
 
         def test_new_window(self, driver):
             new_window_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
             new_window_page.open()
             text_result = new_window_page.check_opened_new_window()
-            assert text_result == "This is a sample page", 'text_result is not equal text_title'
+            assert text_result == "This is a sample page", 'The new window has not opened or an incorrect window'
 
     class TestAlerts:
         
@@ -37,9 +37,20 @@ class TestAlertsFrameWindow:
             alert_text = alerts_page.check_confirm_alert()
             assert alert_text == "You selected Ok", "Allert did not show up"
 
-        @pytest.mark.smoke
+        
         def test_promt_alert(self, driver):
             alerts_page = AlertsPage(driver, 'https://demoqa.com/alerts')
             alerts_page.open()
             text, alert_text = alerts_page.check_prompt_alert()
             assert alert_text == f"You entered {text}", "Allert did not show up"
+
+    class TestFramesPage:
+        @pytest.mark.smoke
+        def test_frames(self, driver):
+            frame_page = FramesPage(driver, 'https://demoqa.com/frames')
+            frame_page.open()
+            result_frame1 = frame_page.check_frame('frame1')
+            result_frame2 = frame_page.check_frame('frame2')
+            assert result_frame1 == ['This is a sample page', '500px', '350px'], 'The frame does not exist'
+            assert result_frame2 == ['This is a sample page', '100px', '100px'], 'The frame does not exist'
+            
