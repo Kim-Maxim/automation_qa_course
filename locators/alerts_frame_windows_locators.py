57747729b511d6f19320fad04308f4422499e5c0
Maxim Kim <kimmaxim69@gmail.com>
2023-11-11 11:12:32 +0300

    Добавил тесты для страницы Alerts

--- locators/alerts_frame_windows_locators.py
+++ locators/alerts_frame_windows_locators.py
@@ -5,4 +5,15 @@ class BrowserWindowsPageLocators:
     NEW_TAB_BUTTON = (By.CSS_SELECTOR, "button[id='tabButton']")
     TITLE_NEW = (By.CSS_SELECTOR, "h1[id='sampleHeading']")
     NEW_WINDOW_BUTTON = (By.CSS_SELECTOR, "button[id='windowButton']")
+
+class AlertsPageLocators:
+    
+    SEE_ALERT_BUTTON = (By.CSS_SELECTOR, "button[id='alertButton']")
+    APPEAR_ALERT_BUTTON_5_SEC_BUTTON = (By.CSS_SELECTOR, "button[id='timerAlertButton']")
+    CONFIRM_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, "button[id='confirmButton']")
+    CONFIRM_RESULT = (By.CSS_SELECTOR, "span[id='confirmResult']")
+    PROMPT_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, "button[id='promtButton']")
+    PROMPT_RESULT = (By.CSS_SELECTOR, "span[id='promptResult']")
+
+
     
--- tests/alerts_frame_windows_tests.py
+++ tests/alerts_frame_windows_tests.py
@@ -1,4 +1,5 @@
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+import pytest
+from pages.alerts_frame_windows_page import AlertsPage, BrowserWindowsPage
 
 
 class TestAlertsFrameWindow:
@@ -15,3 +16,30 @@ class TestAlertsFrameWindow:
             new_window_page.open()
             text_result = new_window_page.check_opened_new_window()
             assert text_result == "This is a sample page", 'text_result is not equal text_title'
+
+    class TestAlerts:
+        
+        def test_see_alert(self, driver):
+            alerts_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alerts_page.open()
+            alert_text = alerts_page.check_see_alert()
+            assert alert_text == "You clicked a button", "The new window has not opened or an incorrect window"
+                
+        def test_see_alert(self, driver):
+            alerts_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alerts_page.open()
+            alert_text = alerts_page.check_alert_appear_5_sec()
+            assert alert_text == "This alert appeared after 5 seconds", "Allert did not show up"
+
+        def test_confirm_alert(self, driver):
+            alerts_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alerts_page.open()
+            alert_text = alerts_page.check_confirm_alert()
+            assert alert_text == "You selected Ok", "Allert did not show up"
+
+        @pytest.mark.smoke
+        def test_promt_alert(self, driver):
+            alerts_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alerts_page.open()
+            text, alert_text = alerts_page.check_prompt_alert()
+            assert alert_text == f"You entered {text}", "Allert did not show up"
