3ccb3707e6e6dada4a3289f89d5352e7bd140c03
Maxim Kim <kimmaxim69@gmail.com>
2023-11-15 09:53:38 +0300

    Добавил класс и тесты для Tabs

--- pages/widgets_page.py
+++ pages/widgets_page.py
@@ -3,7 +3,7 @@ import time
 
 from generator.generator import generated_color, generated_date
 from pages.base_page import BasePage
-from locators.widgets_page_locators import AccordianPageLocators, AutoCompletePageLocators, DatePickerPageLocators, ProgressBarPageLocators, SliderPageLocators
+from locators.widgets_page_locators import AccordianPageLocators, AutoCompletePageLocators, DatePickerPageLocators, ProgressBarPageLocators, SliderPageLocators, TabsPageLocators
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import Keys
 from selenium.webdriver.support.select import Select
@@ -136,3 +136,27 @@ class ProgressBarPage(BasePage):
         progress_bar_button.click()        
         value_after = self.element_is_present(self.locators.PROGRESS_BAR_VALUE).text
         return value_before, value_after
+    
+class TabsPage(BasePage):
+    
+    locators = TabsPageLocators()
+
+    def check_tabs(self, name_tab):
+        tabs = {"what": 
+                     {'title':self.locators.TABS_WHAT,
+                      'content': self.locators.TABS_WHAT_CONTENT},
+                "origin": 
+                     {'title':self.locators.TABS_ORIGIN,
+                      'content': self.locators.TABS_ORIGIN_CONTENT},
+                "use": 
+                     {'title':self.locators.TABS_USE,
+                      'content': self.locators.TABS_USE_CONTENT},
+                "more": 
+                     {'title':self.locators.TABS_MORE,
+                      'content': self.locators.TABS_MORE_CONTENT}
+                      }
+        button = self.element_is_visible(tabs[name_tab]['title'])
+        button.click()
+        what_content = self.element_is_visible(tabs[name_tab]['content']).text
+        return button.text, len(what_content)
+        
--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,6 +1,6 @@
 import time
 import pytest
-from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, ProgressBarPage, SliderPage
+from pages.widgets_page import AccordianPage, AutoCompletePage, DatePickerPage, ProgressBarPage, SliderPage, TabsPage
 
 
 class TestWidgets:
@@ -64,9 +64,24 @@ class TestWidgets:
             assert before != after, "The slider value has not been changed"
                 
     class TestProgressBarPage:
-        @pytest.mark.smoke
+        
         def test_progress_bar(self, driver):
             progress_bar = ProgressBarPage(driver, 'https://demoqa.com/progress-bar')
             progress_bar.open()
             before, after = progress_bar.change_progress_bar_value()
             assert before != after, "The progress bar value has not been changed"
+
+    class TestTabsPage:
+        @pytest.mark.smoke
+        def test_tabs(self, driver):
+            tabs = TabsPage(driver, 'https://demoqa.com/tabs')
+            tabs.open()
+            what_button, what_content = tabs.check_tabs('what')
+            origin_button, origin_content = tabs.check_tabs('origin')
+            use_button, use_content = tabs.check_tabs('use')
+            # more_button, more_content =tabs.check_tabs('more')
+            assert what_button == 'What' and what_content != 0, 'The tab was not pressed or the text is missing'
+            assert origin_button == 'Origin' and origin_content != 0, 'The tab was not pressed or the text is missing'
+            assert use_button == 'Use' and use_content != 0, 'The tab was not pressed or the text is missing'
+            # assert more_button == 'More' and more_content != 0, 'The tab was not pressed or the text is missing'
+
