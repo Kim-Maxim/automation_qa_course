3839db47401206d911e52b7bf0a08376cfc1d95b
Maxim Kim <kimmaxim69@gmail.com>
2023-12-23 14:18:32 +0300

    Закомментил версию браузера в conftest.py

--- conftest.py
+++ conftest.py
@@ -5,14 +5,22 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope = 'function')
+
+@pytest.fixture(scope="function")
 def driver():
-    options = Options()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.page_load_strategy = 'eager'
-    driver = webdriver.Chrome(options=options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--log-level=1")
+    # chrome_options.set_capability("browserVersion", "118")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    chrome_options.page_load_strategy = "eager"
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     yield driver
     attach = driver.get_screenshot_as_png()
-    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
+    allure.attach(
+        attach,
+        name=f"Screenshot {datetime.today()}",
+        attachment_type=allure.attachment_type.PNG,
+    )
     driver.quit()
